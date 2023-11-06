750f53db9db186602ad98db75ef937b7f993775a
faishal <faishalfarras.bts@gmail.com>
2023-11-06 11:26:46 +0700

    change header logo image dir path

--- src/components/NoteAppHeader.jsx
+++ src/components/NoteAppHeader.jsx
@@ -5,7 +5,7 @@ function NoteAppHeader({ searchNote }) {
   return (
     <div className="bg-neutral-800 shadow-md flex sm:flex-row xs:flex-col xs:gap-4 justify-between items-center px-20 xs:px-10 p-3 w-full sticky top-0">
       <div className="flex gap-x-4">
-        <img src="/solar_notes-bold-duotone.svg" alt="" />
+        <img src="solar_notes-bold-duotone.svg" alt="" />
         <h1 className="text-2xl font-bold">Notes App</h1>
       </div>
       <NoteSearch searchNote={searchNote} />
